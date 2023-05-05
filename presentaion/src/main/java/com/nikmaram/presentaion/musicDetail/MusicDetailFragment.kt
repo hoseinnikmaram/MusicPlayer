676b3976c25df7f6077db6f388cf11fab17e625d
nikmara <mhnikmaram1@gmail.com>
2023-05-05 23:39:51 +0330

    [ADD] implement of Music Detail

---
+++ presentaion/src/main/java/com/nikmaram/presentaion/musicDetail/MusicDetailFragment.kt
@@ -0,0 +1,44 @@
+package com.nikmaram.presentaion.musicDetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.nikmaram.presentaion.databinding.FragmentDetailMusicBinding
+
+class MusicDetailFragment : Fragment() {
+
+    private var binding: FragmentDetailMusicBinding? = null
+    private val args: MusicDetailFragmentArgs by navArgs()
+    private val viewModel: MusicDetailViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDetailMusicBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val musicId = args.musicId
+        viewModel.loadMusicById(musicId)
+        viewModel.music.observe(viewLifecycleOwner) { music ->
+            binding?.apply {
+                // set text and image views with music data
+                binding?.model = music
+                binding?.lifecycleOwner = lifecycleOwner
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
---
+++ presentaion/src/main/java/com/nikmaram/presentaion/musicDetail/MusicDetailViewModel.kt
@@ -0,0 +1,31 @@
+package com.nikmaram.presentaion.musicDetail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nikmaram.data.model.MusicFile
+import com.nikmaram.data.utility.ResultData
+import com.nikmaram.domain.useCase.GetMusicFileByIdUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+@HiltViewModel
+class MusicDetailViewModel @Inject constructor(
+    private val getMusicByIdUseCase: GetMusicFileByIdUseCase
+) : ViewModel() {
+
+    private val _music = MutableLiveData<MusicFile>()
+    val music: LiveData<MusicFile> = _music
+
+    fun loadMusicById(musicId: Long) {
+        viewModelScope.launch {
+            when (val result = getMusicByIdUseCase(musicId)) {
+                is ResultData.Success -> _music.postValue(result.data as MusicFile?)
+                is ResultData.Error -> {
+                    // handle error result
+                }
+            }
+        }
+    }
+}
