00ac5d5c9897e953b74dfe112183dd816472dffe
nikmara <mhnikmaram1@gmail.com>
2023-05-05 17:48:12 +0330

    [ADD] imageUri in MusicFile.kt

--- data/src/main/java/com/nikmaram/data/model/MusicFile.kt
+++ data/src/main/java/com/nikmaram/data/model/MusicFile.kt
@@ -1,10 +1,13 @@
 package com.nikmaram.data.model
 
+import android.net.Uri
+
 data class MusicFile(
     val id: Int,
     val title: String,
     val artist: String,
-    val album: String,
+    val album: String?,
     val duration: Long,
-    val filePath: String
+    val filePath: String,
+    val imageUri: Uri? = null
 )
