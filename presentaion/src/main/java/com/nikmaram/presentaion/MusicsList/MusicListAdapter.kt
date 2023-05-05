aed915d10d195d625975bc52824605c21020c7d5
nikmara <mhnikmaram1@gmail.com>
2023-05-05 22:39:20 +0330

    [ADD] MusicListAdapter.kt

---
+++ presentaion/src/main/java/com/nikmaram/presentaion/MusicsList/MusicListAdapter.kt
@@ -0,0 +1,44 @@
+package com.nikmaram.presentaion.MusicsList
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.nikmaram.data.model.MusicFile
+import com.nikmaram.presentaion.databinding.ListItemMusicBinding
+
+class MusicListAdapter(private val onMusicFileClicked: (MusicFile) -> Unit) :
+    ListAdapter<MusicFile, MusicViewHolder>(MusicFileDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemMusicBinding.inflate(inflater, parent, false)
+        return MusicViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MusicViewHolder, position: Int) {
+        val musicFile = getItem(position)
+        holder.bind(musicFile)
+        holder.itemView.setOnClickListener { onMusicFileClicked(musicFile) }
+    }
+}
+
+class MusicViewHolder(private val binding: ListItemMusicBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(musicFile: MusicFile) {
+        binding.music = musicFile
+        binding.executePendingBindings()
+    }
+}
+
+class MusicFileDiffCallback : DiffUtil.ItemCallback<MusicFile>() {
+    override fun areItemsTheSame(oldItem: MusicFile, newItem: MusicFile): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: MusicFile, newItem: MusicFile): Boolean {
+        return oldItem == newItem
+    }
+}
