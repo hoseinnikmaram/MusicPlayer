9c207cacdde6ab3ae78f4044db5840715515b829
nikmara <mhnikmaram1@gmail.com>
2023-05-09 17:17:25 +0330

    [ADD] implementation of MusicPlayerService.kt

--- data/src/main/java/com/nikmaram/data/model/MusicFile.kt
+++ data/src/main/java/com/nikmaram/data/model/MusicFile.kt
@@ -2,7 +2,10 @@ package com.nikmaram.data.model
 
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class MusicFile(
     val id: Long,
     val title: String,
@@ -11,4 +14,4 @@ data class MusicFile(
     val duration: Long,
     val filePath: String,
     val imageUri: Bitmap? = null
-)
+): Parcelable
