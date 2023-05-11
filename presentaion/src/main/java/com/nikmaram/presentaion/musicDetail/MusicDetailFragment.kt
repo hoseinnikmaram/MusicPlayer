2ba701304a0f2124071296944e1ff4cc971c50e1
nikmara <mhnikmaram1@gmail.com>
2023-05-11 19:18:57 +0330

    [FIX] play and pause MusicDetailFragment

--- presentaion/src/main/java/com/nikmaram/presentaion/musicDetail/MusicDetailFragment.kt
+++ presentaion/src/main/java/com/nikmaram/presentaion/musicDetail/MusicDetailFragment.kt
@@ -5,22 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
-import com.nikmaram.data.model.MusicFile
+import com.bumptech.glide.Glide
+import com.google.android.exoplayer2.MediaMetadata
+import com.google.android.exoplayer2.Player
+import com.google.android.material.slider.Slider
 import com.nikmaram.presentaion.R
 import com.nikmaram.presentaion.databinding.FragmentDetailMusicBinding
 import com.nikmaram.presentaion.service.MusicPlayerService
+import com.nikmaram.presentaion.utility.formatAsDate
+import com.nikmaram.presentaion.utility.formatForDisplaying
+import com.nikmaram.presentaion.utility.setOnBackFragmentNavigation
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class MusicDetailFragment : Fragment() {
+class MusicDetailFragment : Fragment(),View.OnClickListener{
 
     private var binding: FragmentDetailMusicBinding? = null
-    private val args: MusicDetailFragmentArgs by navArgs()
-    @Inject
-    lateinit var musicPlayerService: MusicPlayerService
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -32,42 +32,92 @@ class MusicDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val musicFile = args.musicFile
-            binding?.apply {
-                // set text and image views with music data
-                binding?.model = musicFile
-                binding?.lifecycleOwner = lifecycleOwner
-                binding?.executePendingBindings()
+        MusicPlayerService.getCurrentMetadata().observe(viewLifecycleOwner) {
+            if (it != null && binding != null)
+                updateUI(it)
         }
-        binding?.playPauseButton?.setOnClickListener { onPlayButtonClicked() }
-        binding?.nextButton?.setOnClickListener { onNextButtonClicked() }
-        binding?.prevButton?.setOnClickListener { onPreviousButtonClicked() }
+        binding!!.ibPlayPause.setOnClickListener(this)
+        binding!!.ibNextTrack.setOnClickListener(this)
+        binding!!.ibPreviousTrack.setOnClickListener(this)
+        setupTimeSlider()
+        setCurrentMetadataObserver()
+        setIsPlayingObserver()
+        setTimelineObserver()
     }
-    private fun onPlayButtonClicked() {
-        val isPlaying = musicPlayerService.togglePlayback()
-        binding?.playPauseButton?.setImageResource(
-            if (isPlaying) R.drawable.ic_play
-            else R.drawable.ic_pause
-        )
+    private fun setupTimeSlider() {
+        binding!!.sliderTime.addOnChangeListener { slider, value, fromUser ->
+            if (fromUser)
+                MusicPlayerService.onSeekTo(value.toLong())
+        }
+    }
+    private fun setIsPlayingObserver() = MusicPlayerService.isPlaying().observe(viewLifecycleOwner) {
+        if (it) {
+            binding!!.ibPlayPause.setImageResource(R.drawable.ic_baseline_pause_circle_filled_64)
+        } else {
+            binding!!.ibPlayPause.setImageResource(R.drawable.ic_baseline_play_circle_filled_64)
+        }
+    }
+    private fun setTimelineObserver() = MusicPlayerService.getCurrentDuration().observe(viewLifecycleOwner) {
+
+        val duration = MusicPlayerService.getDuration()
+
+        if (duration < 0 || it < 0)
+            return@observe
+
+        binding!!.sliderTime.apply {
+            valueFrom = 0f
+            valueTo = (duration).toFloat()
+            value = (it).toFloat()
+        }
+
+        binding!!.tvCurrentTrackTime.text = it.formatAsDate()
+        binding!!.tvRemainingTrackTime.text = (duration - it).formatAsDate()
     }
 
-    private fun onNextButtonClicked() {
-        val musicFile = musicPlayerService.playNext()
-        updateUI(musicFile)
+    private fun setCurrentMetadataObserver() = MusicPlayerService.getCurrentMetadata().observe(viewLifecycleOwner) {
+        if (it != null)
+            updateUI(it)
     }
 
-    private fun onPreviousButtonClicked() {
-        val musicFile = musicPlayerService.playPrevious()
-        updateUI(musicFile)
-    }
+    private fun updateUI(mediaMetadata: MediaMetadata) {
 
-    private fun updateUI(musicFile: MusicFile?) {
-        binding?.model = musicFile
-        binding?.executePendingBindings()
+        binding!!.sliderTime.setLabelFormatter {
+            (it.toLong()).formatAsDate()
+        }
+        binding!!.tvTrackName.text = mediaMetadata.title.formatForDisplaying()
+        binding!!.tvPerformer.text = mediaMetadata.artist.formatForDisplaying()
+
+        val bytes = mediaMetadata.artworkData
+
+        setupImage(bytes)
+    }
+    private fun setupImage(bytes: ByteArray?) {
+
+        Glide.with(this)
+            .load(bytes)
+            .placeholder(
+                    R.drawable.ic_round_audiotrack_24
+            )
+            .into(binding!!.ivCover)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
     }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            binding!!.ibPlayPause.id -> {
+                MusicPlayerService.togglePlayback()
+            }
+            binding!!.ibNextTrack.id -> {
+                MusicPlayerService.onNext()
+            }
+            binding!!.ibPreviousTrack.id -> {
+                MusicPlayerService.onPrevious()
+            }
+        }
+    }
+
 }
--- presentaion/src/main/java/com/nikmaram/presentaion/utility/extension.kt
+++ presentaion/src/main/java/com/nikmaram/presentaion/utility/extension.kt
@@ -4,7 +4,18 @@ import android.content.Intent
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.Parcelable
-
+import androidx.navigation.findNavController
+import com.google.android.material.appbar.MaterialToolbar
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+private const val PATTERN_1 = "m:ss"
+private const val PATTERN_2 = "mm:ss"
+private const val PATTERN_3 = "H:mm:ss"
+private const val PATTERN_4 = "HH:mm:ss"
+private const val TEN_MINUTES: Long = 600000
+private const val ONE_HOUR: Long = 3600000
+private const val TEN_HOURS: Long = 36000000
 inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
     SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
     else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
@@ -23,4 +34,36 @@ inline fun <reified T : Parcelable> Bundle.parcelableArrayList(key: String): Arr
 inline fun <reified T : Parcelable> Intent.parcelableArrayList(key: String): ArrayList<T>? = when {
     SDK_INT >= 33 -> getParcelableArrayListExtra(key, T::class.java)
     else -> @Suppress("DEPRECATION") getParcelableArrayListExtra(key)
+}
+
+
+internal fun Long.formatAsDate(): String {
+    return when (this) {
+        in 0..TEN_MINUTES -> SimpleDateFormat(PATTERN_1, Locale.getDefault()).format(Date(this))
+        in TEN_MINUTES..ONE_HOUR -> SimpleDateFormat(PATTERN_2, Locale.getDefault()).format(Date(this))
+        in ONE_HOUR..TEN_HOURS -> SimpleDateFormat(PATTERN_3, Locale.getDefault()).format(Date(this))
+        else -> SimpleDateFormat(PATTERN_4, Locale.getDefault()).format(Date(this))
+    }
+}
+
+internal fun CharSequence?.formatForDisplaying(): CharSequence {
+    return if(this.isNullOrBlank())
+        "Unknown"
+    else if(this.isNullOrEmpty())
+        "Unknown"
+    else
+        this
+}
+
+internal fun Short.toSliderValue(): Float {
+    return (this / 100).toFloat()
+}
+
+internal fun Float.toAdjustableValue(): Short {
+    return (this * 100).toInt().toShort()
+}
+internal fun MaterialToolbar.setOnBackFragmentNavigation() {
+    this.setNavigationOnClickListener {
+        it.findNavController().popBackStack()
+    }
 }
