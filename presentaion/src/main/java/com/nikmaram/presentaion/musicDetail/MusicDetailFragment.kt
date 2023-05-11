18fbd0b572ee3769e041bf6cf76c1be49056fe30
nikmara <mhnikmaram1@gmail.com>
2023-05-11 15:24:32 +0330

    [ADD] play pause to MusicDetailFragment.kt

--- presentaion/src/main/java/com/nikmaram/presentaion/musicDetail/MusicDetailFragment.kt
+++ presentaion/src/main/java/com/nikmaram/presentaion/musicDetail/MusicDetailFragment.kt
@@ -7,16 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.nikmaram.data.model.MusicFile
+import com.nikmaram.presentaion.R
 import com.nikmaram.presentaion.databinding.FragmentDetailMusicBinding
+import com.nikmaram.presentaion.service.MusicPlayerService
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MusicDetailFragment : Fragment() {
 
     private var binding: FragmentDetailMusicBinding? = null
     private val args: MusicDetailFragmentArgs by navArgs()
-    private val viewModel: MusicDetailViewModel by viewModels()
-
+    @Inject
+    lateinit var musicPlayerService: MusicPlayerService
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,15 +32,38 @@ class MusicDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val musicId = args.musicId
-        viewModel.loadMusicById(musicId)
-        viewModel.music.observe(viewLifecycleOwner) { music ->
+        val musicFile = args.musicFile
             binding?.apply {
                 // set text and image views with music data
-                binding?.model = music
+                binding?.model = musicFile
                 binding?.lifecycleOwner = lifecycleOwner
-            }
+                binding?.executePendingBindings()
         }
+        binding?.playPauseButton?.setOnClickListener { onPlayButtonClicked() }
+        binding?.nextButton?.setOnClickListener { onNextButtonClicked() }
+        binding?.prevButton?.setOnClickListener { onPreviousButtonClicked() }
+    }
+    private fun onPlayButtonClicked() {
+        val isPlaying = musicPlayerService.togglePlayback()
+        binding?.playPauseButton?.setImageResource(
+            if (isPlaying) R.drawable.ic_play
+            else R.drawable.ic_pause
+        )
+    }
+
+    private fun onNextButtonClicked() {
+        val musicFile = musicPlayerService.playNext()
+        updateUI(musicFile)
+    }
+
+    private fun onPreviousButtonClicked() {
+        val musicFile = musicPlayerService.playPrevious()
+        updateUI(musicFile)
+    }
+
+    private fun updateUI(musicFile: MusicFile?) {
+        binding?.model = musicFile
+        binding?.executePendingBindings()
     }
 
     override fun onDestroyView() {
