dc786914f25c47aa5dfc8bfabe32d0814e90f5d2
nikmara <mhnikmaram1@gmail.com>
2023-05-19 11:02:32 +0330

    [ADD] provide database

--- data/src/main/java/com/nikmaram/data/di/DataModule.kt
+++ data/src/main/java/com/nikmaram/data/di/DataModule.kt
@@ -1,20 +1,39 @@
 package com.nikmaram.data.di
 
 import android.content.Context
+import androidx.room.Room
+import com.nikmaram.data.dao.MusicFileDao
 import com.nikmaram.data.dataSource.MusicDataSource
 import com.nikmaram.data.dataSource.MusicDataSourceImp
+import com.nikmaram.data.db.MusicDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataModule {
 
     @Provides
-    fun provideMusicDataSource(@ApplicationContext context: Context): MusicDataSource {
-        return MusicDataSourceImp(context)
+    fun provideMusicDataSource(@ApplicationContext context: Context,musicFileDao: MusicFileDao): MusicDataSource {
+        return MusicDataSourceImp(context,musicFileDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMusicDatabase(@ApplicationContext context: Context): MusicDatabase {
+        return Room.databaseBuilder(
+            context,
+            MusicDatabase::class.java,
+            "music_database"
+        ).build()
+    }
+
+    @Provides
+    fun provideMusicFileDao(database: MusicDatabase): MusicFileDao {
+        return database.musicFileDao()
     }
 }
