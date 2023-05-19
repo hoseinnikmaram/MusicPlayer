96bd4f846fd965ad9bf1a43d230c16f7e125161b
nikmara <mhnikmaram1@gmail.com>
2023-05-19 11:08:24 +0330

    [ADD] MusicFileMapper.kt

---
+++ data/src/main/java/com/nikmaram/data/mappers/MusicFileMapper.kt
@@ -0,0 +1,35 @@
+package com.nikmaram.data.mappers
+
+import com.nikmaram.data.model.MusicFile
+import com.nikmaram.data.model.MusicFileEntity
+
+
+fun MusicFileEntity.toMusicFile():MusicFile {
+    return MusicFile(
+        id,
+        title,
+        artist,
+        album,
+        duration,
+        filePath
+    )
+}
+
+fun MusicFile.toMusicFileEntity():MusicFileEntity {
+    return MusicFileEntity(
+        id,
+        title,
+        artist,
+        album,
+        duration,
+        filePath
+    )
+}
+
+fun List<MusicFileEntity>.toMusicFileList(): List<MusicFile> {
+    val returnableList = mutableListOf<MusicFile>()
+    for (n in this.indices) {
+        returnableList.add(this[n].toMusicFile())
+    }
+    return returnableList
+}
