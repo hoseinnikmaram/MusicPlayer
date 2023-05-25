0e7ffa55c2518af286af8ba7e42766cf2d27d0d7
nikmara <mhnikmaram1@gmail.com>
2023-05-25 11:08:08 +0330

    [ADD] first store to database then get data as paging

--- data/src/main/java/com/nikmaram/data/dataSource/MusicDataSourceImp.kt
+++ data/src/main/java/com/nikmaram/data/dataSource/MusicDataSourceImp.kt
@@ -1,28 +1,36 @@
 package com.nikmaram.data.dataSource
 
 import android.content.Context
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
 import com.nikmaram.data.dao.MusicFileDao
 import com.nikmaram.data.mappers.toMusicFile
 import com.nikmaram.data.mappers.toMusicFileEntity
 import com.nikmaram.data.model.MusicFile
 import com.nikmaram.data.utility.MusicUtil
-import com.nikmaram.data.utility.ResultData
-import javax.inject.Inject
-import javax.inject.Singleton
-
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 
 
 class MusicDataSourceImp (private val context:Context,private val musicFileDao: MusicFileDao) :MusicDataSource {
-    override suspend fun getMusicFiles(): List<MusicFile>? {
-        if (musicFileDao.getMusicFilesCount() == 0){
+    override suspend fun getMusicFilesAsPaging(): Flow<PagingData<MusicFile>> {
+        if (musicFileDao.getMusicFilesCount() == 0) {
             MusicUtil.getAllMusicFiles(context)?.let { musics ->
                 musicFileDao.insertMusicsFile(
                     musics.map { it.toMusicFileEntity() }
                 )
             }
         }
-        return musicFileDao.getAllMusicFiles()?.map { it.toMusicFile() }
+        return Pager(
+            config = PagingConfig(15, enablePlaceholders = false)
+        ) {
+            musicFileDao.getAllMusicFilesAsPaging()
+        }.flow.map { list -> list.map { it.toMusicFile() } }
     }
+
 
     override suspend fun getMusicFileById(id: Long): MusicFile? {
         return musicFileDao.getMusicFileById(id)?.toMusicFile()
